828a49d6df7a65cd276f88009bce3784233c38b2
Ludovico Tamigio <ludovico.tamigio@crispybacon.it>
2020-03-10 10:16:05 +0100

    feat: build unit test (jest+enzyme) configuration

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
+import { shallow} from 'enzyme';
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const wrapper = shallow(<App/>);
+
+describe('App component', () => {
+    it('renders learn react link', () => {
+        expect(wrapper.find('.App-link')).toHaveLength(1);
+    });
 });
